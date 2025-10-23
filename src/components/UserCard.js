43d7509fab89985f84c7f30b492ed9d91e3d16e6
Brandon2401 <dikirrbrandon@gmail.com>
2025-10-23 21:50:43 +0300

    completed labs

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,9 +1,13 @@
-
+import {link} from "react-router-dom";
 
 function UserCard({user}) {
+  console.log(user);
   return (
     <article>
         <h2>{user.name}</h2>
+        <p>
+          <Link to={'/profile/${user.id}'}>View profile</Link>
+        </p>
     </article>
   );
 };
